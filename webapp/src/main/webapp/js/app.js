949365a973629f21361fe86636ab6908bac1bf82
apfadler <andreas.pfadler@gmail.com>
2016-01-19 00:04:09 +0100

    [POC-3] Webapp structure and css

--- webapp/src/main/webapp/js/app.js
+++ webapp/src/main/webapp/js/app.js
@@ -10,9 +10,21 @@ var app = angular.module('app', [
 app.config(['$routeProvider',
             function ($routeProvider) {
                $routeProvider
-                     .when('/', {
-                              templateUrl: 'partials/home.html',
-                              controller: 'HomeController'
+                      .when('/', {
+                              templateUrl: 'partials/dashboard.html',
+                              controller: 'DashboardController'
+                           })
+					  .when('/data', {
+                              templateUrl: 'partials/data_management.html',
+                              controller: 'DataController'
+                           })
+					  .when('/tasks', {
+                              templateUrl: 'partials/task_management.html',
+                              controller: 'TaskController'
+                           })
+					  .when('/cluster', {
+                              templateUrl: 'partials/cluster_status.html',
+                              controller: 'ClusterController'
                            })
                      .otherwise({
                                    redirectTo: '/'
--- webapp/src/main/webapp/js/controllers.js
+++ webapp/src/main/webapp/js/controllers.js
@@ -2,6 +2,33 @@
 
 var controllers = angular.module("controllers", []);
 
-controllers.controller("HomeController", ['$scope', function ($scope) {
+controllers.controller("DashboardController", ['$scope', function ($scope) {
    $scope.title = 'Hello world!';
+}]);
+
+
+controllers.controller("DataController", ['$scope', function ($scope) {
+   $scope.title = 'Hello world!';
+   
+   $scope.dataServices = [
+	{ id : "Name", "type" : "Document Cache", size : 36 }
+   ]
+   
+}]);
+
+controllers.controller("TaskController", ['$scope', function ($scope) {
+   $scope.title = 'Hello world!';
+   
+   $scope.tasks = [
+	{ name : "Name", status : "Running" , result : ""}
+   ]
+}]);
+
+controllers.controller("ClusterController", ['$scope', function ($scope) {
+   $scope.title = 'Hello world!';
+   
+   
+    $scope.clusterNodes = [
+	{ id : "Name", cpu : 30.0 , mem : 100.0, nocores : 4, activeJobs : 23 }
+   ]
 }]);
