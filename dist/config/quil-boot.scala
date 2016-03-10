71d73f78938ee1baabd4cec8d19c178d4d55a5fe
apfadler <andreas.pfadler@gmail.com>
2016-03-10 16:33:25 +0100

    [POC-MOCO] Last changes before merge into master

--- dist/config/quil-boot.scala
+++ dist/config/quil-boot.scala
@@ -1,8 +1,19 @@
-import org.quil.server.Deployments
+import org.quil.server.ObjectIndex
+import java.io.File
+import org.apache.commons.io.FilenameUtils;
 
-println("Initial Deployment...")
+println("Loading Market Data...")
+ObjectIndex.add("SimpleCache", "Markets", "MarketData.Today", "/MoCo.MarketData.xml")
 
-Deployments.add("SimpleCache", "MoCoMarkets", "MarketData.Today", "/MoCo.MarketData.xml")
-Deployments.add("SimpleCache", "Templates", "MoCo.PlainVanillaSwaption", "/Template.MoCo.PlainVanillaSwaption.xml")
+println("Loading Templates...")
+ObjectIndex.add("SimpleCache", "Templates", "MoCo.PlainVanillaSwaption", "/Template.MoCo.PlainVanillaSwaption.xml")
 
-println("...done")
+
+println("Importing trades...")
+for (file <- new File(System.getenv("QUIL_HOME")+"/sampledata/Trades").listFiles) { 
+  var filename = FilenameUtils.getBaseName(file.toString())
+  println("Adding trade " + filename)
+  ObjectIndex.add("DocumentCache", "Trades", filename, "/Trades/"+filename+".json")
+}
+println("done")
+
